e6d65c705db53c40e393d01e47c904c69d9a1c39
Sweeper777 <sumulang@gmail.com>
2018-03-31 08:44:22 +0100

    initial commit

---
+++ OperationJSONGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperationJSONGenerator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
