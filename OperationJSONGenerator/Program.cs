b1162d5bce72dde8f3f4b641c7b7384b867cf381
Sweeper777 <sumulang@gmail.com>
2018-04-01 08:44:11 +0100

    check file existence

--- OperationJSONGenerator/Program.cs
+++ OperationJSONGenerator/Program.cs
@@ -12,6 +12,11 @@ namespace OperationJSONGenerator
                 Console.WriteLine("Please enter the file path to read: (base directory - Desktop)");
                 filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/" + Console.ReadLine();
                 fileName = Path.GetFileNameWithoutExtension(filePath);
+                if (!File.Exists(filePath)) {
+                    Console.WriteLine("File does not exist!");
+                    Console.WriteLine();
+                    continue;
+                }
                 break;
             }
         }
